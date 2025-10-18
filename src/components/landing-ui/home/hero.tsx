858b6c02108eb917bc12361393d99e03636e93db
vedant ghodki <vedantghodki@gmail.com>
2025-10-18 12:57:01 +0530

    add navbar and hero in landing-ui

--- src/components/landing-ui/home/hero.tsx
+++ src/components/landing-ui/home/hero.tsx
@@ -2,6 +2,7 @@ import { Newsreader } from "next/font/google";
 import Link from "next/link";
 import React from "react";
 import { cn } from "@/lib/utils";
+import { NeumorphButton } from "../button";
 
 const newsreader = Newsreader({
   subsets: ["latin"],
@@ -13,25 +14,16 @@ export const Hero = () => {
       <h2
         className={`${newsreader.className} text-center text-7xl leading-16 tracking-tight`}
       >
-        <span className="bg-gradient-to-r from-[#19BBEC] via-[#1266F7] to-[#18A0DE] bg-clip-text text-transparent">
-          Life's
+        <span className="bg-gradient-to-br from-[#70D9FF] to-[#00A4DF] bg-clip-text text-transparent">
+          Interactivity
         </span>{" "}
-        too <br /> short for <span className="italic">bad UI</span>
+        at your <br /> finger-tips.
       </h2>
-      <p className="text-1xl text-center text-neutral-500">
-        The only component library you'll need <br /> Because life's too short
-        for bad UI just Copy paste and chill
+      <p className="text-1xl mt-4 text-center text-neutral-400">
+        The only shadcn component library <br /> you'll ever need to make your
+        website interactive and seamless.
       </p>
-      <Link
-        className={cn(
-          "mt-6 rounded-md border border-none bg-gray-950 px-4 py-2 text-sm text-white transition-all duration-200 ease-in-out dark:bg-gray-300 dark:text-neutral-800 dark:hover:bg-gray-100",
-          "shadow-[0_3px_10px_rgb(0,0,0,0.2)]",
-          "dark:shadow-[0_3px_10px_rgba(255,255,255,0.1)]",
-        )}
-        href="/components"
-      >
-        Browse Components
-      </Link>
+      <NeumorphButton>Browse Components</NeumorphButton>
     </div>
   );
 };
